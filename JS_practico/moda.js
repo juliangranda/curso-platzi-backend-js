298369ee51294edecc2a375cd352c683ce733c45
juliangranda <juliangranda@hotmail.com>
2021-11-25 13:01:49 -0500

    moda con html

--- JS_practico/moda.js
+++ JS_practico/moda.js
@@ -13,11 +13,26 @@
 
 ]; */
 
-function calcularModa(lista1){
+let lista = [];
+
+
+function añadirDato(){
+ 
+  const inputList = document.getElementById("InputDato")
+  const list = inputList.valueAsNumber; 
+  lista.push(list);
+  const resultadoLista = document.getElementById("resultadoLista");
+  resultadoLista.innerText = "los datos de la lista son: " + lista;
+  return lista;
+}
+
+
+
+function calcularModa(/*lista1*/){
     const lista1Count = {};
     
     //herramienta de array map: map itera sus elementos en orden de insercion un bucle for que devuelve una matriz de [key,value] para cada iteracion.
-    lista1.map(
+    lista.map(
         function(elemento){
             if(lista1Count[elemento]){
                 lista1Count[elemento] += 1;
@@ -35,6 +50,8 @@ function calcularModa(lista1){
     );
 
     const moda = lista1Array[lista1Array.length - 1];
+    const resultadoModa = document.getElementById("ResultP");
+    resultadoModa.innerText ="la moda de la lista es: " + moda[0] + " y se repite " + moda[1] + "veces";
     return moda;
 }
 
